e3e09d03db346310fd89f66f9f1ecef837a252f6
hugntran <133189997+hugntran@users.noreply.github.com>
2024-04-13 11:16:38 +0700

    Update BookFactory.php

--- BookFactory.php
+++ BookFactory.php
@@ -24,7 +24,6 @@ class BookFactory extends Factory
             "ISBN" => $name,
             "pub_year" => $this->faker->year('now'), 
             "available" => $this->faker->randomDigitNotNull,
-
         ];
     }
 }
