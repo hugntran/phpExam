9c49d3261310105595b00193d029cf289f6864b2
hugntran <133189997+hugntran@users.noreply.github.com>
2024-04-13 10:20:50 +0700

    Update 2024_04_13_013141_create_table_library.php

--- 2024_04_13_013141_create_table_library.php
+++ 2024_04_13_013141_create_table_library.php
@@ -12,16 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
-            // Định nghĩa cột bookid với kiểu bigIncrements và là khóa chính
             $table->bigIncrements("bookid");
-            // Định nghĩa cột authorid với kiểu integer
             $table->integer("authorid");
-            // Định nghĩa các cột khác
             $table->string("title", 55);
             $table->string("ISBN", 25);
             $table->smallInteger("pub_year");
             $table->tinyInteger("available");
-            // Các cột timestamps
             $table->timestamps();
         });
     }
